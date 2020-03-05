18d6d92af53470c7254dc31d6b74e043d151e2ae
Heesu1 <puwamwa43@gmail.com>
2020-03-05 08:42:20 -0800

    chore: initialied files

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import title from './Title'
+
+export default function Title({title}) {
+    return (
+        <div className="section-title">
+            <h4>{title}</h4>
+        </div>
+    )
+}
+
+
